44406814a8acea5bac1952263029b02fef3264e0
Shantanu <shantanu22100@iiitnr.edu.in>
2025-02-17 18:30:54 +0530

    db url for local and docker separated

--- web-app/backend/config/db.js
+++ web-app/backend/config/db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const {Pool} = pg;
 const pool = new Pool({
-    connectionString: process.env.DOCKER_DB_URL,
+    connectionString: process.env.DB_URL, //use DOCKER_DB_URL when running through a docker container
 });
 
 export default pool;
